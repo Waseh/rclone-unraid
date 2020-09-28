754e796a6c637141196708dcc31663e1cbbe3c90
Waseh <Waseh@users.noreply.github.com>
2020-09-28 23:36:23 +0200

    Code cleanup and various optimizations

--- source/rclone/usr/local/emhttp/plugins/rclone/updaterclone.sh
+++ source/rclone/usr/local/emhttp/plugins/rclone/updaterclone.sh
@@ -6,58 +6,38 @@ else
    rcloneurl="https://downloads.rclone.org/rclone-current-linux-amd64.zip"
 fi;
 
-ping -q -c2 8.8.8.8 >/dev/null
+version=`rcloneorig --version | head -n 1`
+
+ping -q -c3 8.8.8.8 >/dev/null || ping -q -c3 1.1.1.1 >/dev/null
 if [ $? -eq 0 ]
 then
-  echo "Downloading rclone"
-  curl --connect-timeout 15 --retry 3 --retry-delay 2 --retry-max-time 30 -o /boot/config/plugins/rclone/install/rclone-current.zip $rcloneurl
+  echo "Updating rclone"
+  curl --connect-timeout 15 --retry 3 --retry-delay 2 --retry-max-time 30 -o /boot/config/plugins/rclone/install/rclone.zip $rcloneurl
+  unzip -o -j "/boot/config/plugins/rclone/install/rclone.zip" "*/rclone" -d "/boot/config/plugins/rclone/install"
+  rm -f /boot/config/plugins/rclone/install/*.zip
+  cp /boot/config/plugins/rclone/install/rclone /usr/sbin/rcloneorig.new
+  mv /usr/sbin/rcloneorig.new /usr/sbin/rcloneorig
+  chown root:root /usr/sbin/rcloneorig
+  chmod 755 /usr/sbin/rcloneorig
 else
-  ping -q -c2 1.1.1.1 >/dev/null
-  if [ $? -eq 0 ]
-  then
-    echo "Downloading rclone"
-    curl --connect-timeout 15 --retry 3 --retry-delay 2 --retry-max-time 30 -o /boot/config/plugins/rclone/install/rclone-current.zip $rcloneurl
-  else
-    echo "No internet - Could not fetch new version"
-    exit 1
-  fi
-fi;
-
-if [ -f /boot/config/plugins/rclone/install/rclone-current.zip ]; then
-  unzip /boot/config/plugins/rclone/install/rclone-current.zip -d /boot/config/plugins/rclone/install/
-  rm -f $(ls /boot/config/plugins/rclone/install/rclone-current.old.zip 2>/dev/null)
-  mv /boot/config/plugins/rclone/install/rclone-current.zip /boot/config/plugins/rclone/install/rclone-current.old.zip
-fi;
-
-if [ -f /boot/config/plugins/rclone/install/rclone-v*/rclone ]; then
-  mv /boot/config/plugins/rclone/install/rclone-v*/rclone  /usr/sbin/rcloneorig
-  if [ "$?" -ne "0" ]; then
-	echo ""
-	echo "-------------------------------------------------------------------"
-	echo "<font color='red'> Update failed, rclone binary couldn't be replaced. Please try again</font>"
-	echo "-------------------------------------------------------------------"
-	echo ""
-    if [ -d /boot/config/plugins/rclone/install/rclone-v*/ ]; then
-      rm -rf /boot/config/plugins/rclone/install/rclone-v*/
-    fi;
-    exit 1
-  fi;
-else
-	echo ""
-	echo "-------------------------------------------------------------------"
-	echo "<font color='red'> Unpack failed. Please try again</font>"
-	echo "-------------------------------------------------------------------"
-	echo ""
-  rm -f $(ls /boot/config/plugins/rclone/install/rclone-current.old.zip 2>/dev/null)
+  echo ""
+  echo "-------------------------------------------------------------------"
+  echo "<font color='red'> No internet - Can't fetch new version </font>"
+  echo "-------------------------------------------------------------------"
+  echo ""
   exit 1
 fi;
 
-if [ -d /boot/config/plugins/rclone/install/rclone-v*/ ]; then
-  rm -rf /boot/config/plugins/rclone/install/rclone-v*/
-fi;
+current_version=`rcloneorig --version | head -n 1`
 
-chown root:root /usr/sbin/rcloneorig
-chmod 755 /usr/sbin/rcloneorig
+if [[ $version = $current_version ]]; then
+  echo ""
+  echo "-------------------------------------------------------------------"
+  echo "<font color='red'> Update failed - Please try again </font>"
+  echo "-------------------------------------------------------------------"
+  echo ""
+  exit 1
+fi;
 
 echo ""
 echo "-----------------------------------------------------------"
